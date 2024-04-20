ec3eb2e993dce15c66f8329257bb1f6fe1afc05e
spikal2000 <spikal1997@gmail.com>
2024-04-20 23:20:50 +0300

    cleaneing repo

--- KalogerisPortal_J/src/main/java/com/kalogerisportal/kalogerisportal_j/constents/BakeryConstants.java
+++
@@ -1,9 +0,0 @@
-package com.kalogerisportal.kalogerisportal_j.constents;
-
-public class BakeryConstants {
-
-    public static final String SOMETHING_WENT_WRONG = "something went wrong.";
-
-    public static final String INVALID_DATA = "invalid data";
-
-}
