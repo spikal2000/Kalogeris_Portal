1f8cbd42fc107de6b756434d31f32b96b31d509a
spikal2000 <73543919+spikal2000@users.noreply.github.com>
2024-09-24 14:58:52 +0300

    Update settings.py

--- KalogerisPortal/KalogerisPortal/settings.py
+++ KalogerisPortal/KalogerisPortal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l#ps262o&==(t1sm!bd7r5jgh!7@h_b_&f#%vk$t$div#=er#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
